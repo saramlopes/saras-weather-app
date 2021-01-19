7a91b1b631cd03367aa56870b5a3ba22ce016954
saramlopes <saramlopes@hotmail.co.uk>
2021-01-19 13:22:07 +0000

    Fixed unit error

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function showTemperature(response) {
 	//wind
 	let wind = document.querySelector("#wind");
 	let _wind = response.data.wind.speed;
-	wind.innerHTML = `${_wind}MPH`;
+	wind.innerHTML = `${_wind}km/h`;
 
 	//humidity
 	let humidity = document.querySelector("#humidity");
